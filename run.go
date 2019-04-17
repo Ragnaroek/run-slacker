95816caaf1fe58258703a0eace3082e926af9937
Michael Bohn <michael.bohn@freiheit.com>
2019-04-17 20:17:02 +0200

    correctly encode slack payload

--- run.go
+++ run.go
@@ -1,55 +1,74 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
-  "os/exec"
-  "fmt"
-  "net/http"
-  "strings"
+	"os/exec"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
 
 type Slack struct {
-  Hook string `toml:"hook"`
+	Hook string `toml:"hook"`
 }
 
 type Config struct {
-  Prog string
-  Args []string
-  Slack Slack
+	Dir   string
+	Prog  string
+	Args  []string
+	Slack Slack
 }
 
 func main() {
 
-  if len(os.Args) != 2 {
-    panic(fmt.Sprintf("No config file given"))
-  }
+	if len(os.Args) != 2 {
+		panic(fmt.Sprintf("No config file given"))
+	}
 
-  confFile := os.Args[1]
+	confFile := os.Args[1]
 	config := Config{}
 	_, err := toml.DecodeFile(confFile, &config)
 	if err != nil {
-    //TODO slack panic
+		//TODO slack panic
 		panic(fmt.Sprintf("Error reading config file: %#v", err))
 	}
 
-   out, err := exec.Command(config.Prog, config.Args...).CombinedOutput()
-   if err != nil {
-     //TODO slack panic
-     panic(fmt.Sprintf("Error running program: %#v", err))
-   }
+	cmd := exec.Command(config.Prog, config.Args...)
+	cmd.Dir = config.Dir
+	out, err := cmd.CombinedOutput()
 
-   slack(&config, string(out))
+	if err != nil {
+		//TODO slack panic
+		panic(fmt.Sprintf("Error running program: %#v", err))
+	}
+
+	slack(&config, string(out))
+}
+
+type slackPayload struct {
+	Text string `json:"text"`
 }
 
 func slack(conf *Config, msg string) {
-  payload := fmt.Sprintf("{\"text\": \"%s\"}", msg)
-	resp, err := http.Post(conf.Slack.Hook, "application/json", strings.NewReader(payload))
-  if err != nil {
-    panic(fmt.Sprintf("Unexpected error from slack %#v", resp))
-  }
+	payload := slackPayload{Text: msg}
+	payloadStr, err := json.Marshal(payload)
+	if err != nil {
+		panic(fmt.Sprintf("Could not create slack message: %#v", err))
+	}
+	resp, err := http.Post(conf.Slack.Hook, "application/json", strings.NewReader(string(payloadStr)))
+	if err != nil {
+		panic(fmt.Sprintf("Unexpected error from slack: %#v", resp))
+	}
 	if resp.StatusCode != 200 {
-		panic(fmt.Sprintf("Got unexpected response from slack %#v", resp))
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		body := string(bodyBytes)
+		if err != nil {
+			body = fmt.Sprintf("err reading body from response: %#v", resp)
+		}
+		panic(fmt.Sprintf("Got unexpected response from slack: %s", body))
 	}
 }
