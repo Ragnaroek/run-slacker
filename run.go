5202a15fc6c75fce3a50cc2a58153e32028b91ce
Michael Bohn <michael.bohn@freiheit.com>
2019-06-06 05:44:50 +0200

    improved err message

--- run.go
+++ run.go
@@ -44,11 +44,12 @@ func main() {
 	cmd.Dir = config.Dir
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		outMessage := fmt.Sprintf("%#v\n-------\n%s", err, string(out))
 		if exitError, ok := err.(*exec.ExitError); ok {
+			outMessage := fmt.Sprintf("%#v\n-------\n%s", exitError.Error(), string(out))
 			exitCode := exitError.ExitCode()
 			slack(&config, runFailedMessage(&config, &exitCode, outMessage))
 		} else {
+			outMessage := fmt.Sprintf("%#v\n-------\n%s", err, string(out))
 			slack(&config, runFailedMessage(&config, nil, outMessage))
 		}
 		return
